c4caca8ae41ba423f6e35a26ce30ecaf3431bad1
Ethan Wessel <ejwessel@gmail.com>
2019-09-29 14:03:30 -0400

    move default construction of context into index

--- src/dashboard/context.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const Context = createContext({});
-
-export const Provider = Context.Provider;
--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,26 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ReactDOM from "react-dom";
-import { ethers } from 'ethers';
-import { Context, Provider } from './context';
-import { Tribute } from './Tribute';
 import Dashboard from "./Dashboard";
 
-//Set Injection Bindings Here:
-//Views
-//Controllers
-const context = {
-  tribute: tribute
-}
-
-useEffect(() => {
-  //create the contract here using ethers
-  const walletProvider = new ethers.providers.Web3Provider(web3.currentProvider);
-  const contactAddress = "";
-  const rDAIContract = new ethers.ContractFactory(contractAddress, abi, walletProvider)
-  const tribute = new Tribute(rDAIContract, walletProvider);
-}, []);
-
-
+//Create Default Values for Context and set Injection Bindings
+let Context = createContext({
+  tribute: null,
+  isConnected: false
+  // set injection Views
+  // set injection Controllers
+});
+const Provider = Context.Provider;
 
 let App = document.getElementById('app');
 ReactDOM.render(
