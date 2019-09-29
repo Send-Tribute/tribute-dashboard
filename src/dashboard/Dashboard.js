b4d57a893b205dbcae89b5704725d35713c924cd
Ethan Wessel <ejwessel@gmail.com>
2019-09-29 18:43:30 -0400

    checkpoint

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -16,7 +16,29 @@ export default function Dashboard() {
 
   const [context, setContext] = useContext(Context);
 
+  function getViews() {
+    return Object.assign({},
+      { Footer },
+      { Header },
+    )
+  }
+
+  function getControllers() {
+    return Object.assign({},
+      { Footer },
+      { Header },
+    )
+  }
+
   useEffect(() => {
+
+    let views = getViews();
+    let controllers = getControllers();
+    let tribute = null;
+    let isConnected = false;
+
+    console.log(context);
+
     try {
       if (typeof window.ethereum !== 'undefined'
         || (typeof window.web3 !== 'undefined')) {
@@ -24,43 +46,32 @@ export default function Dashboard() {
         // Web3 browser user detected. You can now use the provider.
         let walletProvider = window['ethereum'] || window.web3.currentProvider;
         walletProvider = new ethers.providers.Web3Provider(walletProvider);
-        let tribute = null;
         //const contactAddress = ""; //READ FROM FILE
         //const rDAIContract = new ethers.ContractFactory(contractAddress, abi, walletProvider);
-        //const tribute = new Tribute(rDAIContract, walletProvider);
-        const isConnected = true;
-        setContext(state => {
-            return Object.assign({}, {tribute}, {isConnected})
-        });
+        tribute = new Tribute("", walletProvider);
+        isConnected = true;
       }
+
+      setContext(state => {
+        return Object.assign(
+          { views },
+          { controllers },
+          { tribute },
+          { isConnected},
+        )
+      });
     } catch (error) {
       console.log(error);
     }
-  }, []);
+  }, [context.isConnected]);
 
-  //let [selectedTab, setSelectedTab] = useState();
-
-  //use state, context or redux. We plan on using state since it's not necessary to use context
-
-  //function here that renders Wallet, Sending, Receiving, and Settings based on state
-  //pass the setSelectedTab Header
-  //
-  //the problem is that all the data between the views is different. How do we obtain that data and pass it
-  //in a generalized way?
-  //
-  //Proposals:
-  //We can use controllers that will fetch and then we can passed the returned data
-  //based upon state call the correct controller to get the correct data
-  //there will be 4 different controllers
-  //
-  //
-  //    <Header/>
-  //    { /* controller call would go here */ }
-  //    <Footer/>
 
   return (
     <div>
       DASHBOARD
+      <Header/>
+      { /* controller call would go here */ }
+      <Footer/>
     </div>
   );
 }
