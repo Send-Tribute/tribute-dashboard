a90d715c67d6d5ac6b64f5b4764977e707ea455d
Ethan Wessel <ejwessel@gmail.com>
2019-09-29 12:11:10 -0400

    Tribute.js Skeleton

---
+++ src/dashboard/Tribute.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { ethers } from 'ethers';
+
+export default function Tribute() {
+
+  async function generateTribute(){
+  }
+
+  async function disableTribute() {
+  }
+
+  async function removeRecipient() {
+  }
+
+  async function addRecipient() {
+  }
+
+  async function modifyRecipient() {
+  }
+
+  async function setTribute() {
+  }
+
+  async function getInterestPayable() {
+  }
+
+  async function claimTribute() {
+  }
+
+  async function isTributeFlowing(){
+  }
+
+  function convertDaiRateToAllocation() {
+  }
+
+  function convertRateToAllocation() {
+  }
+
+  function getLendingRate() {
+  }
+
+  async function generateReport() {
+  }
+}
