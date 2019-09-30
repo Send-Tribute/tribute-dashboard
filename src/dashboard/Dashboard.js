3d5f9d34f5fd4ff9e42b227f021938e2f5d2a57a
Patrick Gallagher <blockchainbuddha@gmail.com>
2019-09-30 00:20:08 -0400

    added tab handler

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,25 +1,28 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { Context } from './context';
 import { ethers } from 'ethers';
 import Tribute from './Tribute';
 
 import Footer from './Footer.js';
 import Header from './Header.js';
+import Sending from './sending/Sending.js';
+import Wallet from './wallet/Wallet.js';
+import Receiving from './receiving/Receiving.js';
+import Settings from './settings/Settings.js';
 //import Table from './Table.js';
-//import Sending from './sending/Sending.js';
-//import Wallet from './wallet/Wallet.js';
-//import Receiving from './receiving/Receiving.js';
-//import Settings from './settings/Settings.js';
 //import './css/outline.css';
 
-export default function Dashboard() {
+import { TABS } from './helpers/general';
 
+const Dashboard = () => {
   const [context, setContext] = useContext(Context);
 
   useEffect(() => {
     try {
-      if (typeof window.ethereum !== 'undefined'
-        || (typeof window.web3 !== 'undefined')) {
+      if (
+        typeof window.ethereum !== 'undefined' ||
+        typeof window.web3 !== 'undefined'
+      ) {
         console.log(window.web3.version);
         // Web3 browser user detected. You can now use the provider.
         let walletProvider = window['ethereum'] || window.web3.currentProvider;
@@ -30,7 +33,7 @@ export default function Dashboard() {
         //const tribute = new Tribute(rDAIContract, walletProvider);
         const isConnected = true;
         setContext(state => {
-            return Object.assign({}, {tribute}, {isConnected})
+          return Object.assign({}, { tribute }, { isConnected });
         });
       }
     } catch (error) {
@@ -38,29 +41,21 @@ export default function Dashboard() {
     }
   }, []);
 
-  //let [selectedTab, setSelectedTab] = useState();
-
-  //use state, context or redux. We plan on using state since it's not necessary to use context
-
-  //function here that renders Wallet, Sending, Receiving, and Settings based on state
-  //pass the setSelectedTab Header
-  //
-  //the problem is that all the data between the views is different. How do we obtain that data and pass it
-  //in a generalized way?
-  //
-  //Proposals:
-  //We can use controllers that will fetch and then we can passed the returned data
-  //based upon state call the correct controller to get the correct data
-  //there will be 4 different controllers
-  //
-  //
-  //    <Header/>
-  //    { /* controller call would go here */ }
-  //    <Footer/>
+  const getContent = () => {
+    const tabName = context.selectedTab ? context.selectedTab : TABS.default;
+    if (tabName === 'sending') return <Sending />;
+    if (tabName === 'receiving') return <Receiving />;
+    if (tabName === 'settings') return <Settings />;
+    return <Wallet />;
+  };
 
   return (
     <div>
-      DASHBOARD
+      <Header />
+      {getContent()}
+      <Footer />
     </div>
   );
-}
+};
+
+export default Dashboard;
---
+++ src/dashboard/helpers/general.js
@@ -0,0 +1,10 @@
+export const TABS = {
+  ordering: ['wallet', 'sending', 'receiving', 'settings'],
+  default: 'sending',
+  displayNames: {
+    wallet: 'Wallet',
+    sending: 'Sending',
+    receiving: 'Receiving',
+    settings: 'Settings'
+  }
+};
