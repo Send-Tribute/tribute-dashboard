463d5c157abd3273df0ba35b651a9a13dbc74c62
brian <bchou515@gmail.com>
2019-10-02 17:17:20 -0400

    dashboard changes for etherswrapper to work

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Context } from './context';
 import { ethers } from 'ethers';
+import DAIabi from '../contracts/dai'; // 0xbF7A7169562078c96f0eC1A8aFD6aE50f12e5A99
+import rDAIabi from '../contracts/rDai'; // 0xea718e4602125407fafcb721b7d760ad9652dfe7
 import Tribute from './Tribute';
 
 import Footer from './Footer.js';
@@ -22,16 +24,26 @@ export default function Dashboard() {
         || (typeof window.web3 !== 'undefined')) {
         console.log(window.web3.version);
         // Web3 browser user detected. You can now use the provider.
-        let walletProvider = window['ethereum'] || window.web3.currentProvider;
-        walletProvider = new ethers.providers.Web3Provider(walletProvider);
-        let tribute = null;
+        // let walletProvider = window['ethereum'] || window.web3.currentProvider;
+        let walletProvider = new ethers.providers.Web3Provider(window.web3.currentProvider);
+        
         //const contactAddress = ""; //READ FROM FILE
-        //const rDAIContract = new ethers.ContractFactory(contractAddress, abi, walletProvider);
-        //const tribute = new Tribute(rDAIContract, walletProvider);
-        const isConnected = true;
-        setContext(state => {
-            return Object.assign({}, {tribute}, {isConnected})
-        });
+        const rDAIAddress = '0xea718e4602125407fafcb721b7d760ad9652dfe7';
+        const DAIAddress = '0xbF7A7169562078c96f0eC1A8aFD6aE50f12e5A99';
+        
+        // connect to contracts on the network
+        const rDAIContract = new ethers.Contract(rDAIAddress, rDAIabi, walletProvider);
+        const DAIContract = new ethers.Contract(DAIAddress, DAIabi, walletProvider);
+        
+        const tribute = new Tribute(DAIContract, rDAIContract, walletProvider);
+
+        console.log(tribute.provider);
+        console.log(tribute.contract);
+        console.log(tribute.signer);
+        // const isConnected = true;
+        // setContext(state => {
+        //     return Object.assign({}, {tribute}, {isConnected})
+        // });
       }
     } catch (error) {
       console.log(error);
