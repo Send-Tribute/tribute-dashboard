d36fbeb2eac85f0e039dcc44714e6aebba18015a
brian <bchou515@gmail.com>
2019-10-03 16:43:42 -0400

    editied function calls

--- src/dashboard/Tribute.js
+++ src/dashboard/Tribute.js
@@ -3,6 +3,7 @@ import 'babel-polyfill';
 import {ethers} from 'ethers';
 const {bigNumberify} = ethers.utils;
 const {WeiPerEther} = ethers.constants;
+
 export default function Tribute (DAIContract, rDAIContract, provider, address) {
     /*
     +  RToken (IRToken, Ownable, ReentrancyGuard)
@@ -56,7 +57,10 @@ export default function Tribute (DAIContract, rDAIContract, provider, address) {
     this.rDAIContract = this.rDAIContract.connect(this.signer);
     this.address = address;
 
-    this.generateUnallocatedTribute = async (amountToTribute) => {
+    // [you are in first]
+    // every time the pricipal changes we make a new hat
+    // add more rdai to your account while updating you hat
+    this.generateTribute = async (amountToTribute) => {
         //provided some amount generate more
         //rDAI so that it can be allocated
         // amountToTribute to big number and in wei
@@ -65,21 +69,8 @@ export default function Tribute (DAIContract, rDAIContract, provider, address) {
         await this.rDAIContract
     }
 
-    this.getRDAI = async (amount) => {
-        console.log(amount);
-        console.log(bigNumberify(amount).mul(WeiPerEther));
-        console.log(rDAIContract.address);
-        await this.DAIContract.approve(rDAIContract.address, bigNumberify(amount).mul(WeiPerEther));
-        await this.rDAIContract.mint(bigNumberify(amount).mul(WeiPerEther));
-        await this.rDAIContract.getHatByAddress(this.address);
-    }
-
-    this.sendTribute = async () => {
-        // begin flowing of tribute from an account to another account
-        await contract.createHat();
-    }
-
-    this.disableTribute = async () => {
+    // reedemm all your rdai to dai
+    this.disableTribute = async (addressToDisable) => {
         //stop flowing of tribute from an account to another account
         //set hat back to 0 hat
         console.log(this.address)
@@ -88,36 +79,42 @@ export default function Tribute (DAIContract, rDAIContract, provider, address) {
         await this.rDAIContract.changeHat(0);
     }
 
-    this.updateTributeAllocations = () => {
-        //this will add, remove, and modify existing amounts
-        //this will allow batch allocation updates
-
-        // let currentHat = await contract.getHatByAddress();
-        //get data from current hat
-        //do math to compute how much everybody else needs to be allocated
-        //create a new hat with MODS to the existing recipients
-        // await contract.createHat();
+    // this function mints rDAI to your account
+    this.getRDAI = async (amount) => {
+        console.log(amount);
+        console.log(bigNumberify(amount).mul(WeiPerEther));
+        console.log(rDAIContract.address);
+        // what happens when approval works but mint gets dropped
+        await this.DAIContract.approve(rDAIContract.address, bigNumberify(amount).mul(WeiPerEther));
+        // dont call newhat
+        await this.rDAIContract.mint(bigNumberify(amount).mul(WeiPerEther));
+        let output = await this.rDAIContract.getHatByAddress(this.address);
+        console.log(output);
     }
 
-    this.getIncomingTribute = async () => {
-        //shows current amount of interest being generated against an account
-        await contract.interestPayableOf();
+    // send and end 
+    this.sendTribute = async (address) => {
+        // begin flowing of tribute from an account to another account
+        await contract.createHat();
     }
-
+    // should take out an address for a hat
+    this.endTribute = async (address) => {
+        // begin flowing of tribute from an account to another account
+        await contract.createHat();
+    }
+    
+    // calling pay interest payable of and converting to rdai
+    // you can claim for others
+    // maybe another function claimonbehalfof
+    // come up with another function for other people
     this.claimTribute = async () => {
         //this cashes out all rDAI in both interest
         //and principal and sends it back to the user
         await contract.redeemAll();
     }
 
-    this.isTributeFlowing = async () => {
-        //if the account has a 0 hat then no tribute is flowing out
-        //
-        //NOTE: check if its possible to have no rDAI, 
-        //but allocate a hat and make it APPEAR like tribute is flowing
-    }
-
 // TODO: work on these after other methods have been set
+
 //   function convertDaiRateToAllocation() {
 //     //compute how much principal is needed for an interest rate and ideal payment
 //     //4000 = 400/.10 => 4k is needed to generate 400 after a year
