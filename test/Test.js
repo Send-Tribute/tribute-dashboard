ca387d1774d00a6aeded2543195485956d60f7b2
Ethan Wessel <ejwessel@gmail.com>
2019-10-27 10:51:14 -0700

    test the calling on testnet inside of mocha tests

--- test/Test.js
+++ test/Test.js
@@ -1,11 +1,28 @@
+const ethers = require('ethers')
+const rDAI = require('../src/contracts/rDai.json')
+
+
 contract('TESTING', async (accounts) => {
+
+  let provider
+  let contract
 
     before(async() => {
       console.log(accounts[0])
+      provider = new ethers.providers.JsonRpcProvider("http://localhost:8545")
+      contract = new ethers.Contract(
+                          "0xeA718E4602125407fAfcb721b7D760aD9652dfe7",
+                          rDAI,
+                          provider.getSigner(0)
+                        )
     });
 
     describe("Test Describe", async() => {
-      it("Test it", async () => {
+      it("getHatByAddress", async () => {
+
+        let val = await contract.getHatByAddress("0xb893D8F6779842959C1dfC3095b1c62ceAA16703")
+        console.log(val)
+
         assert.equal(true, true, "did not pass")
       });
   });
