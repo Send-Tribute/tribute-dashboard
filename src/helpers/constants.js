52be3aacbf24949e318e9f169fb62a54fba302bf
Patrick Gallagher <blockchainbuddha@gmail.com>
2020-01-23 03:42:36 -0500

    update to mcDAI

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -18,7 +18,7 @@ export const TABS = {
 export const CONTRACTS = {
   rtoken: {
     kovan: '0xea718e4602125407fafcb721b7d760ad9652dfe7',
-    homestead: '0xea8b224eDD3e342DEb514C4176c2E72Bcce6fFF9'
+    homestead: '0x261b45D85cCFeAbb11F022eBa346ee8D1cd488c0'
   },
   allocationStrategy: {
     kovan: '0xb4377efc05bd28be8e6510629538e54eba2d74e3'
@@ -28,7 +28,7 @@ export const CONTRACTS = {
   },
   dai: {
     kovan: '0xbF7A7169562078c96f0eC1A8aFD6aE50f12e5A99',
-    homestead: '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359'
+    homestead: '0x6B175474E89094C44Da98b954EedeAC495271d0F'
   }
 };
 
